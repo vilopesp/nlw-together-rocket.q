1e73678414b50a3bd0adf41266f88fb08814e210
vilopesp <vilopes1@hotmail.com>
2021-06-26 16:51:16 -0300

    modal buttons script finished

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,20 +2,35 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll(".question-actions a.check") 
-
 checkButtons.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 // MODAL FOR DELETE BUTTON
 const deleteButton = document.querySelectorAll(".question-actions a.delete")
-
 deleteButton.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open()
-    })
-})
+    button.addEventListener("click", (event) => handleClick(event, false))
+ })
+
+function handleClick(event, check = true){
+    event.preventDefault()
+
+    const description = check ? "Tem certeza que deseja" : "excluir esta pergunta?"
+
+    modalTitle.innerHTML = check ? "Marcar como lida": "Excluir esta pergunta"
+
+    modalDescription.innerHTML = `${description} marcar como lida esta pergunta?`
+
+    modalButton.innerHTML = check ? "Marcar como lida" : "Sim, excluir"
+
+    check ? modalButton.style.backgroundColor = "var(--blue)" : modalButton.style.backgroundColor = "var(--pink)"
+
+    modal.open()
+}
+
