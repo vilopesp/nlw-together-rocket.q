9a7cccbbccf4514ac968437b969594659224aacb
vilopesp <vilopesp@hotmail.com>
2021-06-30 09:42:14 -0300

    questionControllers

--- src/route.js
+++ src/route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const questionController = require('./controllers/questionController')
 
 const route = express.Router()
 
@@ -6,7 +7,7 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
-
+route.post('/room/:room/:question/:action', questionController.index)
 
 module.exports = route
 
